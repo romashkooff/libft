d89f5e6781b9d53fbcbe1dfd62129fc7eeedb6c0
romashko <romashko@duck>
2024-12-01 18:28:26 +0100

    Added test for libft

---
+++ tests/main.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romashko <romashko@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/01 18:13:33 by romashko          #+#    #+#             */
+/*   Updated: 2024/12/01 18:26:10 by romashko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "include/ft_printf.h"
+#include "include/get_next_line.h"
+#include "include/libft.h"
+
+int	main(void)
+{
+	char	*line;
+	int		fd;
+	int		i;
+
+	fd = open("test.txt", O_RDONLY);
+	i = 0;
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (!line)
+		{
+			free(line);
+			break ;
+		}
+		i++;
+		ft_printf("This is line №%d: %s", i, line);
+		free(line);
+	}
+	close(fd);
+}
